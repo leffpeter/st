d7997de0ce0319f2abcdace7334dc1c6f655840f
Pedro <leffpeter@gmail.com>
2020-10-09 11:32:24 +0200

    fixes

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Noto Sands Mono:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
